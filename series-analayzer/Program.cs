f52b8960855c86c0731a86e95567ca00809188db
Haim Assraf <haimassraf@gmail.com>
2025-05-12 13:00:30 +0300

    d

---
+++ series-analayzer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace series_analayzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("The git hub work!");
+        }
+    }
+}
